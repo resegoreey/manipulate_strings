f3a69a1be9cfbee9070e2389249faff1f6e4e17d
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2025-01-30 11:19:10 +0200

    Added code for finding the index of the first occurence of a character in a string

--- strings.py
+++ strings.py
@@ -12,3 +12,19 @@ indice_2 = int(input("Enter second indice: "))
 #extract the substring using the given indices
 extraction = string[indice_1:indice_2]
 print(f"Extracted substring is: {extraction}")
+print()
+
+"""
+Find character index
+"""
+#gets a string from the user
+user_string = input("Enter a string: ")
+
+#finds the index of the first occurence of a character in a string
+
+#find the first character
+str_character = user_string[0]
+
+#find the index
+str_index = user_string.find(str_character)
+print(f"The first character is on index: {str_index}")
